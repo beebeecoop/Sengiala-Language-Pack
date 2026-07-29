5bff0d60d9191444527981d01e3aa09ec9b3d11d
Yuslaimi's <koperasibelatabelantaraberhad@gmail.com>
2026-07-29 21:49:37 +0800

    chore: add translation statistics scaffold

---
+++ tools/statistics.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Sengiala Language Pack statistics scaffold.
+ * Coverage reporting will be implemented in a later pull request.
+ */
+
+declare(strict_types=1);
+
+fwrite(STDOUT, "Sengiala language statistics scaffold.\n");
