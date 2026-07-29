ce0a5603c0a4868f5327392913fb52e9dbf55814
Yuslaimi's <koperasibelatabelantaraberhad@gmail.com>
2026-07-29 21:49:28 +0800

    chore: add language validator scaffold

---
+++ tools/validate-language.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Sengiala Language Pack validator scaffold.
+ * Validation rules will be implemented in a later pull request.
+ */
+
+declare(strict_types=1);
+
+fwrite(STDOUT, "Sengiala language validator scaffold.\n");
